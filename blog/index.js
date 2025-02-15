d1010982d9894a785e82c046a5897cf6114c48ca
kdesign25 <lol12002@byui.edu>
2025-02-14 21:43:08 -0500

    blog part 2

--- blog/index.js
+++ blog/index.js
@@ -1,18 +1,33 @@
 function generateFeed(){
     var feed  = document.getElementById("feed")
+    var articleList = ""
+    var newArticle = ""
     for (var i=0; i < articles.length; i++) {
         //Information Box
         // This is the main post
          var title = articles[i]["title"] 
-         var art = document.createElement("article") 
-         var img = document.createElement("img")  
-         art.innerHTML = `<h1>${title}</h1>`
-         img.src = articles[i]["imgSrc"] 
-         art.appendChild(img)
-         var p = document.createElement("p")
-         p.innerText = articles[i]["description"]
-         art.appendChild(p)
-         feed.appendChild(art)   
-
-    }
+         var date = articles[i]["date"] 
+         var description = articles[i]["description"] 
+         var imgSrc = articles[i]["imgSrc"] 
+         var imgAlt = articles[i]["imgAlt"] 
+         var ages = articles[i]["ages"] 
+         var genre = articles[i]["genre"] 
+         var stars = articles[i]["stars"]            
+        newArticle = `<article>
+            <ul class="data">
+                <li class="date">${date}</li>
+                <li>${ages}</li>
+                <li>${genre}</li>
+                <li>${stars}</li>
+            </ul>
+        <p class="bookmain">
+            <h1>${title}</h1>
+            <figure><img src="${imgSrc}" alt="${imgAlt}"></figure> 
+            <p>${description}</p>
+        </p>
+        </article>    `
+        articleList = `${articleList}\n${newArticle}`
+    } 
+    feed.innerHTML = articleList
 }
+
