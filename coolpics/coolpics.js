4ab358100b1e5c1aecb93f7bc57903fa7c7cc90b
kdesign25 <lol12002@byui.edu>
2025-02-22 23:32:59 -0500

    credit card

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -2,11 +2,22 @@ const menuButton = document.querySelector('#menu-button');
 const menu = document.querySelector('.menu');
 
 function toggleMenu() { 
-  console.log("hello")
-    if (menu.classList.contains("hide")){
-      menu.classList.remove('hide')
-    } else {
-      menu.classList.add('hide')
-    } 
+  if (menu.classList.contains("hide")){
+    menu.classList.remove('hide')
+  } else {
+    menu.classList.add('hide')
+  } 
+}
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
   }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
 menuButton.onclick = toggleMenu;
+
